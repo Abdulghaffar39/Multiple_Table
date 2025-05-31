3078ce1654eb1b6a0406768c7df8c5934bdf4504
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-05-31 19:20:10 +0500

    completed table

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let input = document.querySelector('#input')
 let head_1 = document.querySelector('#head_1')
 
@@ -8,13 +9,22 @@ function result() {
     
     
     for(let i = 1; i <= input.value; i++){
-        head_1.innerHTML += (`Table of ${i}<br />`);
+        head_1.innerHTML += `<br /> Table of ${i}  <br /> `;
+        head_1.style.backgroundColor = `black`;
     
         for(let j = 1; j <= 10; j++){
-            head_1.innerHTML += (`${i} X ${j} = ${i * j} <br />`)
+            head_1.innerHTML += `${i} X ${j} = ${i * j}  <br />`;
         }
     }
+    
 
+    input.value = ``;
 }
 
 
+function reset() {
+  console.log('kajsfhkjds');
+  
+    input.value = "";
+    head_1.innerHTML = "";
+  }
